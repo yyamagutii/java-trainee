2f07516edd86ba904d62d165ae5fec01653381eb
F6322775 Lucas Yamaguti Da Costa <lucasycosta@bb.com.br>
2025-08-12 08:54:42 -0300

    arrays 1

---
+++ src/main/java/br/com/bb/pps/domain/AulaLista.java
@@ -0,0 +1,35 @@
+package br.com.bb.pps.domain;
+
+public class AulaLista {
+
+    private String nome;
+    private int idade;
+
+    /*
+    public AulaLista(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+     */
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + nome + "\nIdade: " + idade + "\n";
+    }
+}
---
+++ src/main/java/br/com/bb/pps/exec/AulaListaExec.java
@@ -0,0 +1,50 @@
+package br.com.bb.pps.exec;
+
+import br.com.bb.pps.domain.AulaLista;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AulaListaExec {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //CRIAÇÃO DE UM ARRAYLIST
+        ArrayList<AulaLista> listaUsuario = new ArrayList<>();
+
+        int qtdNome = 2;
+
+        for(int i = 1; i <= qtdNome; i++){
+
+            AulaLista lista = new AulaLista();
+
+            System.out.println("nome: ");
+            String nome = input.nextLine();
+            lista.setNome(nome);
+
+            System.out.println("idade: ");
+            int idade = input.nextInt();
+            input.nextLine();
+            lista.setIdade(idade);
+
+            //ADICIONANDO ELEMENTOS AO ARRAY
+            listaUsuario.add(lista);
+        }
+
+        //IMPRIMIR TODA A LISTA
+        System.out.println(listaUsuario + "\n -------------------");
+        //IMPRIMIR O TAMANHO DA LISTA
+        System.out.println("tamanho da lista: " + listaUsuario.size() + "\n -------------------");
+        //IMPRIMIR UM REGISTRO INTEIRO POR INDICE
+        System.out.println("registro 1: \n" + listaUsuario.get(0) + "\n -------------------");
+        //IMPRIMIR O PRIMEIRO NOME INDICE DA LISTA
+        System.out.println("primeiro usuario: \n" + listaUsuario.get(0).getNome() + "\n -------------------");
+        //IMPRIMIR A SEGUNDA IDADE POR INDICE
+        System.out.println("segunda idade: \n" + listaUsuario.get(1).getIdade() + "\n -------------------");
+
+    }
+
+}
